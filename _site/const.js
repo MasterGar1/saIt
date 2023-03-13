bd00901d1d6c04ef6df7605f493a54bc4c91f184
Gavril <gari@abv.bg>
2023-03-13 12:20:07 +0200

    Jekyll added

---
+++ _site/const.js
@@ -0,0 +1,20 @@
+const ASincrease = 0.5;
+const AtKincrease = 3;
+const HPincrease = 15;
+const ALincrease = 8;
+const AWincrease = 4;
+
+const hp = document.getElementById("hp");
+hp.textContent += HPincrease;
+
+const atk = document.getElementById("atk");
+atk.textContent += AtKincrease;
+
+const as = document.getElementById("as");
+as.textContent += ASincrease;
+
+const aw = document.getElementById("aw");
+aw.textContent += AWincrease;
+
+const al = document.getElementById("al");
+al.textContent += ALincrease;
